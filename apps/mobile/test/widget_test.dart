3d9b2aa47f7107d3c4afad6be31e296e8b6ff58e
slikrapstelimrec123 <slikrapstelimrec123@gmail.com>
2026-07-14 16:18:07 +0300

    Update app widget test for OAuth links

--- apps/mobile/test/widget_test.dart
+++ apps/mobile/test/widget_test.dart
@@ -1,10 +1,11 @@
+import 'package:app_links/app_links.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:vet_care_mobile/main.dart';
 
 void main() {
   testWidgets('app starts with the configured shell', (tester) async {
-    await tester.pumpWidget(const VetCareApp());
+    await tester.pumpWidget(VetCareApp(appLinks: AppLinks()));
     await tester.pump(const Duration(seconds: 3));
 
     final app = tester.widget<MaterialApp>(find.byType(MaterialApp));
